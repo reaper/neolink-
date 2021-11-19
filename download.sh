4b539e9fb92d0b7a6d3a65aed8fc835b45dd9e76
Pierre FILSTROFF <pierre.filstroff@gmail.com>
2021-11-19 10:11:00 +0100

    Small corrections on download script

--- download.sh
+++ download.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-TARGETPLATFORM=$1
+TARGET_PLATFORM=$1
 FILE_DESTINATION=$2
 
-if [[ "$TARGETPLATFORM" == *"386"* ]]; then
+if [[ "$TARGET_PLATFORM" == *"386"* ]]; then
   FILE_SUFFIX="i386-buster"
-elif [[ "$TARGETPLATFORM" == *"amd64"* ]]; then
+elif [[ "$TARGET_PLATFORM" == *"amd64"* ]]; then
   FILE_SUFFIX="ubuntu-18.04"
-elif [[ "$TARGETPLATFORM" == *"arm64"* ]]; then
+elif [[ "$TARGET_PLATFORM" == *"arm64"* ]]; then
   FILE_SUFFIX="arm64-buster"
-elif [[ "$TARGETPLATFORM" == *"arm"* ]]; then
+elif [[ "$TARGET_PLATFORM" == *"arm"* ]]; then
   FILE_SUFFIX="armhf-buster"
 else
-  exit 0
+  exit 1
 fi
 
-wget -O $FILE_DESTINATION "https://nightly.link/thirtythreeforty/neolink/workflows/build/master/release-$FILE_SUFFIX.zip"
+FILE_URL="https://nightly.link/thirtythreeforty/neolink/workflows/build/master/release-$FILE_SUFFIX.zip"
+wget -O $FILE_DESTINATION $FILE_URL
